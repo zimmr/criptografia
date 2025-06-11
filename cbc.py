4677f1128ffba7b07db527a13ed47339260ce99a
zimmr <joaozimmr@gmail.com>
2025-06-10 22:03:05 -0300

    continuacao-2

--- cbc.py
+++ cbc.py
@@ -1,8 +1,17 @@
 import os
 import math
 import sys
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
 
-# Funções
+from base64 import b64encode
+
+# from Crypto.Cipher import AES
+# from Crypto.Util.Padding import pad
+# from Crypto.Random import get_random_bytes
+
+#### Funções ####
 def get_blocks(file_path):
     block_size = 16
 
@@ -20,67 +29,91 @@ def get_blocks(file_path):
 
     return blocks
 
+def format_file_name(file_path, option):
+    sufix = "cifrado" if option == "criptografar" else "decifrado"
+    return file_path.replace(" cifrado", "").replace(" decifrado", "").split(".")[0] + " " + sufix + ".txt" 
+    
 def save_file(encrypted_text):
     with open('encriptado.txt', 'wb') as fbinary:
         fbinary.write(encrypted_text)
     return
 
-def encrypt(file_path):
-    # TODO: dividir em blocos
-    blocks = get_blocks(file_path)
+def encrypt_AES_CBC(data, key, iv):
+    padder = padding.PKCS7(128).padder()
+    padded_data = padder.update(data)
+    padded_data += padder.finalize()
 
-    previous_block = initialization_vector
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    encryptor = cipher.encryptor()
+    ciphertext = encryptor.update(padded_data) + encryptor.finalize()
+    return ciphertext
+
+def decrypt_AES_CBC(ciphertext, key, iv):
+    decryptor = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend()).decryptor()
+    decrypted_data = decryptor.update(ciphertext) + decryptor.finalize()
+
+    unpadder = padding.PKCS7(128).unpadder()
+    unpadded_data = unpadder.update(decrypted_data)
+    unpadded_data += unpadder.finalize()
+    return unpadded_data
+
+def encrypt(file_path, key, iv):
+    blocks = get_blocks(file_path)
+    filteredBlocks = filter(lambda x: not isinstance(x, int), blocks)
+
+    previous_block = iv
     encrypted_text = list()
 
-    # TODO: criptografar blocos
-    for block in blocks:
-        xor_result = int(block) ^ int(previous_block)
-
-        encrypted_block = xor_result ^ cipher_key
+    for block in filteredBlocks:
+        encrypted_block = encrypt_AES_CBC(block, key, previous_block)
         encrypted_text += encrypted_block
-
         previous_block = block
 
     save_file(encrypted_text)
     return
 
-# def encrypt_block(block, previous_block):
-#     return
+def decrypt(file_path, key):
+    blocks = get_blocks(file_path)
+    filteredBlocks = filter(lambda x: not isinstance(x, int), blocks)
+    reverseBlocks = filteredBlocks.reverse()
 
-def decrypt(chunks):
-    print("não implementado.")
+    previous_block = initialization_vector
+    decrypted_text = list()
+
+    for block in reverseBlocks:
+        decrypted_block = decrypt_AES_CBC(block, key, previous_block)
+        decrypted_text += decrypted_block
+        previous_block = block
+
+    save_file(decrypted_text)
     return
 
+#### End funções ####
 
-# def verificar_tamanho_arquivo(file_path):
-#     file_size_bytes = os.path.getsize(file_path)
-#     number_of_chunks = math.ceil(file_size_bytes / 16)
-
-#     print(file_size_bytes)
-#     print(number_of_chunks)
-
-#     return
-
-
-# Script
+#### Script ####
 if (len(sys.argv) < 4):
-    print ("erro")
+    print ("Erro: Argumentos insuficientes.")
     quit()
 
 file_path = sys.argv[1]
-cipher_key = sys.argv[2]
+input_key = sys.argv[2]
 option = sys.argv[3]
+
+# TODO: gerar
 initialization_vector = "teste" # TODO: fazer isso
+cipher_key = algorithms.AES(input_key) # TODO: gerar chave
 
 match option:
     case "criptografar":
         print("criptografando...")
-        encrypt(file_path)
+        encrypt(file_path, cipher_key, initialization_vector)
         quit()
     case "descriptografar":
         print("descriptografando...")
-        decrypt(file_path)
+        decrypt(file_path, cipher_key)
         quit()
     case _:
-        print("erro: opção inválida.")
+        print("Erro: Opção inválida.")
         quit()
+
+### End Script ####
